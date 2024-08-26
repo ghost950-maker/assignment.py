5f58f0a2750aa2e4801109a18e2efac1fac655db
ghost950-maker <alexkinya93@gmail.com>
2024-08-26 11:32:48 +0300

    Add files via upload

---
+++ first.py
@@ -0,0 +1,17 @@
+print("Alex")
+print("16")
+print("I am a footballer")
+print("My favorite colour is red")
+print("My birthplace is Kenya")
+print("I'm a footballer")
+
+
+James = 20
+John = 10
+print(20+10)
+print(20-10)
+print(20*10)
+print(20/10)
+
+age = 21
+print("I will be {age} years old in five years")
